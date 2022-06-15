8f4702304c17dd200ee11e82343dfd58753b18a1
B-Guidi <bguidi@alumnos.exa.unicen.edu.ar>
2022-06-15 13:41:34 -0300

    Defined interface for activity and registration repositories.

--- gym_manager/core/persistence.py
+++ gym_manager/core/persistence.py
@@ -2,7 +2,7 @@ import abc
 from datetime import date
 from typing import Iterable, Generator
 
-from gym_manager.core.base import Client, Activity, Currency, String, Number, Date
+from gym_manager.core.base import Client, Activity, Currency, String, Number, Date, Registration
 
 
 class ClientRepo(abc.ABC):
@@ -42,5 +42,36 @@ class ClientRepo(abc.ABC):
     @abc.abstractmethod
     def all(self, **kwargs) -> Generator[Client, None, None]:
         """Returns all the clients in the repository.
+        """
+        raise NotImplementedError
+
+
+class ActivityRepo(abc.ABC):
+    """Activities repository interface.
+    """
+    @abc.abstractmethod
+    def add(self, name: String, price: Currency, pay_once: bool, description: String) -> int:
+        """Creates an activity with the given data, and returns its id in the repository.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def all(self, **kwargs) -> Generator[Activity, None, None]:
+        raise NotImplementedError
+
+
+class RegistrationRepo(abc.ABC):
+    """Repository interface for client's activities registrations.
+    """
+    @abc.abstractmethod
+    def update_or_create(self, registration: Registration):
+        """Updates the given *registration* in the repository. If there is no row in the repository, then creates a
+        new one.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def expired(self, today: date, **kwargs) -> Iterable[Registration]:
+        """Retrieves all entries whose pay day has passed if today date were *today*.
         """
         raise NotImplementedError
