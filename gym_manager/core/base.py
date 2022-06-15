c71d1c6efa05baed78fec95343c9cd4068db9637
B-Guidi <bguidi@alumnos.exa.unicen.edu.ar>
2022-06-15 13:34:14 -0300

    Implemented core classes. The core classes are Client, Activity, Registration and Payment.

--- gym_manager/core/base.py
+++ gym_manager/core/base.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import abc
+from dataclasses import dataclass, field
 from datetime import date, timedelta, datetime
 from decimal import Decimal
-from typing import Any
+from typing import Any, Iterable, Optional
 
 ONE_MONTH_TD = timedelta(days=30)
 
@@ -120,3 +121,105 @@ class Currency:  # ToDo extend from Validatable.
         return self.amount == o.amount
 
 
+class NotRegistered(KeyError):
+    """Exception thrown when the *client* isn't registered in the *activity*.
+    """
+    def __init__(self, client: Client, activity_id: int, *args: object) -> None:
+        super().__init__(*args)
+        self.client = client
+        self.activity_id = activity_id
+
+    def __str__(self) -> str:
+        return f"The client '{self.client.dni} - {self.client.name}' is not registered in the activity '{self.activity_id}'"
+
+
+@dataclass
+class Activity:
+    """Stores general information about an activity.
+    """
+    id: int
+    name: str
+    price: Currency
+    pay_once: bool
+
+
+@dataclass
+class Client:
+    dni: int
+    name: str = field(compare=False)
+    admission: date = field(compare=False)
+    telephone: str = field(compare=False)
+    direction: str = field(compare=False)
+    _registrations: dict[int, Registration] = field(default_factory=dict, compare=False)
+
+    @property
+    def registrations(self) -> Iterable[Registration]:
+        return self._registrations.values()
+
+    def n_registrations(self) -> int:
+        return len(self._registrations)
+
+    def add_registration(self, registration: Registration):
+        self._registrations[registration.activity.id] = registration
+
+    def record_payment(self, activity: Activity, payment: Payment):
+        """Records the payment of the given *activity*.
+
+        Raises:
+            ValueError if *payment.client* is different from *self.client*.
+            NotRegistered if *self* isn't registered in the *activity*.
+        """
+        if self != payment.client:
+            raise ValueError(f"The client '{payment.client.name}' is paying the activity '{activity.name}' for "
+                             f"the client '{self.name}'.")
+
+        try:
+            entry = self._registrations[activity.id]
+            entry.record_payment(payment)
+            return entry
+        except KeyError as err:
+            raise NotRegistered(self, activity.id) from err
+
+    def registration(self, reg_id: int) -> Registration:
+        try:
+            return self._registrations[reg_id]
+        except KeyError as err:
+            raise NotRegistered(self, reg_id) from err
+
+
+@dataclass
+class Payment:
+    id: int
+    client: Client
+    day: date
+    amount: Currency
+    method: str
+    responsible: str
+    description: str
+
+
+@dataclass
+class Registration:
+    """Stores information about an specific activity done by an specific client.
+    """
+    id: int
+    client: Client
+    activity: Activity
+    payment: Optional[Payment] = None
+
+    def first_pay_missing(self) -> bool:
+        return self.payment is None
+
+    def pay_day_passed(self, today: date) -> bool:
+        return pay_day_passed(self.payment.day, today)
+
+    def record_payment(self, payment: Payment):
+        """Records the payment of the activity.
+
+        Raises:
+            ValueError if *payment.client* is different from *self.client*.
+        """
+        if self.client != payment.client:
+            raise ValueError(f"The client '{payment.client.name}' is paying the activity '{self.activity.name}' for "
+                             f"the client '{self.client.name}'.")
+        self.payment = payment
