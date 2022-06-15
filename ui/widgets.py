9e900489a5e41502091064c46ca88692857e573d
B-Guidi <bguidi@alumnos.exa.unicen.edu.ar>
2022-06-15 14:19:16 -0300

    Implemented Field widget.

---
+++ ui/widgets.py
@@ -0,0 +1,25 @@
+from typing import Type
+
+from PyQt5.QtWidgets import QLineEdit, QWidget
+
+from gym_manager.core.base import Validatable
+
+
+class Field(QLineEdit):
+    def __init__(self, validatable: Type[Validatable], parent: QWidget | None = None, **validate_args) -> None:
+        self.validatable = validatable
+        self.validate_args = validate_args
+        super().__init__(parent)
+        self.base_style_sheet = self.styleSheet()
+
+    def valid_value(self) -> bool:
+        try:
+            self.validatable(self.text(), **self.validate_args)
+            self.setStyleSheet(self.base_style_sheet)
+            return True
+        except (ValueError, TypeError):
+            self.setStyleSheet("border: 1px solid red")
+            return False
+
+    def value(self) -> Validatable:
+        return self.validatable(self.text(), **self.validate_args)
