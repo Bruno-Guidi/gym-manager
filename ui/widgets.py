dd19d58d990f6263815c75239001cf87fc58e184
B-Guidi <bguidi@alumnos.exa.unicen.edu.ar>
2022-06-16 13:52:19 -0300

    Implemented method to validate QTextEdit.

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,8 +1,19 @@
-from typing import Type
+from typing import Type, Any
 
-from PyQt5.QtWidgets import QLineEdit, QWidget
+from PyQt5.QtWidgets import QLineEdit, QWidget, QTextEdit
 
-from gym_manager.core.base import Validatable, ValidationError
+from gym_manager.core.base import Validatable, ValidationError, String
+
+
+def valid_text_value(text: QTextEdit, max_len: int) -> tuple[bool, Any]:
+    valid, value = False, None
+    try:
+        value = String(text.toPlainText(), max_len=max_len)
+        valid = True
+    except ValidationError:
+        pass
+    finally:
+        return valid, value
 
 
 class Field(QLineEdit):
