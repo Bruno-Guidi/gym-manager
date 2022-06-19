7baf4c0b7defaea5eb8ea0b6f2187ad96fda5ab0
B-Guidi <bguidi@alumnos.exa.unicen.edu.ar>
2022-06-19 14:53:47 -0300

    Activity filtering by name working again.

--- ui/activity/main.py
+++ ui/activity/main.py
@@ -7,7 +7,7 @@ from PyQt5.QtWidgets import QMainWindow, QWidget, QListWidget, QHBoxLayout, QLab
 
 from gym_manager.core import constants as consts
 from gym_manager.core.activity_manager import ActivityManager
-from gym_manager.core.base import String, Activity, Currency, TextLike
+from gym_manager.core.base import String, Activity, Currency, TextLike, NameLike
 from ui.activity.create import CreateUI
 from ui.widget_config import config_lbl, config_line, config_btn, config_layout, config_combobox, config_checkbox
 from ui.widgets import Field, valid_text_value, SearchBox
@@ -292,7 +292,7 @@ class ActivityMainUI(QMainWindow):
         self.main_layout.addLayout(self.utils_layout)
         config_layout(self.utils_layout, spacing=0, left_margin=40, top_margin=15, right_margin=80)
 
-        self.search_box = SearchBox([TextLike("name", display_name="Nombre")], parent=self.widget)
+        self.search_box = SearchBox([NameLike("name", display_name="Nombre")], parent=self.widget)
         self.utils_layout.addWidget(self.search_box)
 
         self.search_btn = QPushButton(self.widget)
