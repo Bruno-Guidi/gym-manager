ff2efef4c0e12ed1078b5f418dc18eec96789809
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-02 13:27:44 -0300

    Updated ExtractUI testing script.

--- ui_testing/extract.py
+++ ui_testing/extract.py
@@ -1,13 +1,33 @@
 import sys
+from datetime import date
+from random import random
+from typing import Generator
 
 from PyQt5.QtWidgets import QApplication
 
+from gym_manager.core.base import Transaction, String, Currency, Client
+from gym_manager.core.persistence import TransactionRepo
 from ui.accounting.operations import ExtractUI
+
+
+# noinspection PyShadowingBuiltins
+class Mock(TransactionRepo):
+
+    def from_record(self, id, type, client: Client, when, amount, method, responsible, description):
+        pass
+
+    def create(self, type: String, when: date, amount: Currency, method: String, responsible: String,
+               description: String, client: Client | None = None) -> Transaction:
+        return Transaction(int(random()), type, client, when, amount, method, responsible, description)
+
+    def all(self, page: int, page_len: int = 20, **filters) -> Generator[Transaction, None, None]:
+        pass
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    window = ExtractUI()
+    window = ExtractUI((String("Cash", max_len=15), String("Debit", max_len=15)), Mock())
     window.show()
 
     app.exec()
