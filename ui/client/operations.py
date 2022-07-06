1dc3aede463afdce92c653282cab7231d263e323
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-06 11:43:40 -0300

    CreateUI: adjusted.
    Also checked that charged bookings are underlined in the schedule.

--- ui/client/operations.py
+++ ui/client/operations.py
@@ -4,7 +4,9 @@ import itertools
 from datetime import date
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QHBoxLayout, QVBoxLayout, QLabel, QDateEdit, QPushButton, QComboBox
+from PyQt5.QtWidgets import (
+    QDialog, QHBoxLayout, QVBoxLayout, QLabel, QDateEdit, QPushButton, QComboBox, QGridLayout,
+    QSpacerItem, QSizePolicy)
 
 from gym_manager.core import constants as consts
 from gym_manager.core.base import String, Number, Client, Activity, Subscription
@@ -27,7 +29,7 @@ class CreateController:
 
         # Sets callbacks.
         # noinspection PyUnresolvedReferences
-        self.create_ui.ok_btn.clicked.connect(self.create_client)
+        self.create_ui.confirm_btn.clicked.connect(self.create_client)
         # noinspection PyUnresolvedReferences
         self.create_ui.cancel_btn.clicked.connect(self.create_ui.reject)
 
@@ -42,7 +44,7 @@ class CreateController:
         else:
             self.client = Client(self.create_ui.dni_field.value(),
                                  self.create_ui.name_field.value(),
-                                 self.create_ui.admission_date_field.date().toPyDate(),
+                                 self.create_ui.date_edit.date().toPyDate(),
                                  self.create_ui.tel_field.value(),
                                  self.create_ui.dir_field.value(),
                                  is_active=True)
@@ -58,90 +60,78 @@ class CreateUI(QDialog):
         self.controller = CreateController(self, client_repo)
 
     def _setup_ui(self):
-        self.resize(400, 300)
-
         self.layout = QVBoxLayout(self)
 
-        self.form_layout = QVBoxLayout()
+        # Form.
+        self.form_layout = QGridLayout()
         self.layout.addLayout(self.form_layout)
+        self.form_layout.setContentsMargins(40, 0, 40, 0)
 
         # Name.
-        self.name_layout = QHBoxLayout()
-        self.form_layout.addLayout(self.name_layout)
-        config_layout(self.name_layout, alignment=Qt.AlignLeft)
+        self.name_lbl = QLabel(self)
+        self.form_layout.addWidget(self.name_lbl, 0, 0)
+        config_lbl(self.name_lbl, "Nombre*")
 
-        self.name_lbl = QLabel()
-        self.name_layout.addWidget(self.name_lbl)
-        config_lbl(self.name_lbl, "Nombre", font_size=16, extra_width=120)
-
-        self.name_field = Field(validatable=String, max_len=consts.CLIENT_NAME_CHARS)
-        self.name_layout.addWidget(self.name_field)
-        config_line(self.name_field, place_holder="Nombre", font_size=16)
+        self.name_field = Field(String, parent=self, max_len=consts.CLIENT_NAME_CHARS)
+        self.form_layout.addWidget(self.name_field, 0, 1)
+        config_line(self.name_field, place_holder="Nombre")
 
         # DNI.
-        self.dni_layout = QHBoxLayout()
-        self.form_layout.addLayout(self.dni_layout)
-        config_layout(self.dni_layout, alignment=Qt.AlignLeft)
+        self.dni_lbl = QLabel(self)
+        self.form_layout.addWidget(self.dni_lbl, 1, 0)
+        config_lbl(self.dni_lbl, "DNI*")
 
-        self.dni_lbl = QLabel()
-        self.dni_layout.addWidget(self.dni_lbl)
-        config_lbl(self.dni_lbl, "DNI", font_size=16, extra_width=120)
-
-        self.dni_field = Field(Number, min_value=consts.CLIENT_MIN_DNI, max_value=consts.CLIENT_MAX_DNI)
-        self.dni_layout.addWidget(self.dni_field)
-        config_line(self.dni_field, place_holder="DNI", font_size=16)
+        self.dni_field = Field(Number, parent=self, min_value=consts.CLIENT_MIN_DNI, max_value=consts.CLIENT_MAX_DNI)
+        self.form_layout.addWidget(self.dni_field, 1, 1)
+        config_line(self.dni_field, place_holder="XXXXXXXX")
 
         # Admission.
-        self.admission_layout = QHBoxLayout()
-        self.form_layout.addLayout(self.admission_layout)
-        config_layout(self.admission_layout, alignment=Qt.AlignLeft)
+        self.date_lbl = QLabel(self)
+        self.form_layout.addWidget(self.date_lbl, 2, 0)
+        config_lbl(self.date_lbl, "Ingreso",)
 
-        self.admission_lbl = QLabel()
-        self.admission_layout.addWidget(self.admission_lbl)
-        config_lbl(self.admission_lbl, "Ingreso", font_size=16, extra_width=120)
-
-        self.admission_date_field = QDateEdit()
-        self.admission_layout.addWidget(self.admission_date_field)
-        config_date_edit(self.admission_date_field, date.today(), font_size=16, extra_height=30)
+        self.date_edit = QDateEdit(self)
+        self.form_layout.addWidget(self.date_edit, 2, 1)
+        config_date_edit(self.date_edit, date.today(), calendar=True)
 
         # Telephone.
-        self.tel_layout = QHBoxLayout()
-        self.form_layout.addLayout(self.tel_layout)
-        config_layout(self.tel_layout, alignment=Qt.AlignLeft)
-
         self.tel_lbl = QLabel(self)
-        self.tel_layout.addWidget(self.tel_lbl)
-        config_lbl(self.tel_lbl, "Teléfono", font_size=16, extra_width=120)
+        self.form_layout.addWidget(self.tel_lbl, 3, 0)
+        config_lbl(self.tel_lbl, "Teléfono")
 
-        self.tel_field = Field(validatable=String, optional=consts.CLIENT_TEL_OPTIONAL, max_len=consts.CLIENT_TEL_CHARS)
-        self.tel_layout.addWidget(self.tel_field)
-        config_line(self.tel_field, place_holder="Teléfono", font_size=16)
+        self.tel_field = Field(String, parent=self, optional=consts.CLIENT_TEL_OPTIONAL,
+                               max_len=consts.CLIENT_TEL_CHARS)
+        self.form_layout.addWidget(self.tel_field, 3, 1)
+        config_line(self.tel_field, place_holder="Teléfono")
 
         # Direction.
-        self.dir_layout = QHBoxLayout()
-        self.form_layout.addLayout(self.dir_layout)
-        config_layout(self.dir_layout, alignment=Qt.AlignLeft)
-
         self.dir_lbl = QLabel(self)
-        self.dir_layout.addWidget(self.dir_lbl)
-        config_lbl(self.dir_lbl, "Dirección", font_size=16, extra_width=120)
+        self.form_layout.addWidget(self.dir_lbl, 4, 0)
+        config_lbl(self.dir_lbl, "Dirección")
 
-        self.dir_field = Field(validatable=String, optional=consts.CLIENT_DIR_OPTIONAL, max_len=consts.CLIENT_DIR_CHARS)
-        self.dir_layout.addWidget(self.dir_field)
-        config_line(self.dir_field, place_holder="Dirección", font_size=16)
+        self.dir_field = Field(String, parent=self, optional=consts.CLIENT_DIR_OPTIONAL,
+                               max_len=consts.CLIENT_DIR_CHARS)
+        self.form_layout.addWidget(self.dir_field, 4, 1)
+        config_line(self.dir_field, place_holder="Dirección")
+
+        # Vertical spacer.
+        self.layout.addSpacerItem(QSpacerItem(20, 10, QSizePolicy.Minimum, QSizePolicy.MinimumExpanding))
 
         # Buttons.
         self.buttons_layout = QHBoxLayout()
         self.layout.addLayout(self.buttons_layout)
-        config_layout(self.buttons_layout, alignment=Qt.AlignRight, right_margin=5)
+        self.buttons_layout.setAlignment(Qt.AlignRight)
 
-        self.ok_btn = QPushButton()
-        self.buttons_layout.addWidget(self.ok_btn)
-        config_btn(self.ok_btn, "Ok", extra_width=100)
+        self.confirm_btn = QPushButton(self)
+        self.buttons_layout.addWidget(self.confirm_btn)
+        config_btn(self.confirm_btn, "Confirmar", extra_width=20)
 
-        self.cancel_btn = QPushButton()
+        self.cancel_btn = QPushButton(self)
         self.buttons_layout.addWidget(self.cancel_btn)
-        config_btn(self.cancel_btn, "Cancelar", extra_width=100)
+        config_btn(self.cancel_btn, "Cancelar", extra_width=20)
+
+        # Adjusts size.
+        self.setMaximumSize(self.minimumWidth(), self.minimumHeight())
 
 
 class SubscribeController:
