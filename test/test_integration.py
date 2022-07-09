87657b4c1f9166c6fe9d0c1413c3e9c5e0a456cd
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-08 23:58:45 -0300

    Tested charge(args) function.

--- test/test_integration.py
+++ test/test_integration.py
@@ -51,3 +51,57 @@ def test_cancel():
     # Feature being tested.
     system.cancel(subscription_repo, subscription)
     assert activity_repo.n_subscribers(activity) == 0
+
+
+def test_charge_chargeOnceActivity():
+    # Repositories setup.
+    peewee.create_database(":memory:")
+    activity_repo = peewee.SqliteActivityRepo()
+    transaction_repo = peewee.SqliteTransactionRepo()
+    balance_repo = peewee.SqliteBalanceRepo()
+    client_repo = peewee.SqliteClientRepo(activity_repo, transaction_repo)
+    transaction_repo.client_repo = client_repo  # ToDo after implementing proxies, remove this line.
+    subscription_repo = peewee.SqliteSubscriptionRepo()
+
+    # Data setup.
+    client = Client(Number(1), String("dummy_name", max_len=20), date(2022, 2, 1), String("dummy_tel", max_len=20),
+                    String("dummy_descr", max_len=20), is_active=True)
+    client_repo.add(client)
+
+    activity = Activity(String("dummy_name", max_len=20), Currency(0.0), description=String("dummy_descr", max_len=20),
+                        charge_once=True)
+    activity_repo.add(activity)
+
+    # Feature being tested.
+    system.charge(transaction_repo, subscription_repo, date(2022, 2, 2), client, activity, "dummy_method",
+                  responsible=String("dummy_resp", max_len=20))
+    assert len([t for t in transaction_repo.all()]) == 1
+
+
+def test_charge_notChargeOnceActivity():
+    # Repositories setup.
+    peewee.create_database(":memory:")
+    activity_repo = peewee.SqliteActivityRepo()
+    transaction_repo = peewee.SqliteTransactionRepo()
+    balance_repo = peewee.SqliteBalanceRepo()
+    client_repo = peewee.SqliteClientRepo(activity_repo, transaction_repo)
+    transaction_repo.client_repo = client_repo  # ToDo after implementing proxies, remove this line.
+    subscription_repo = peewee.SqliteSubscriptionRepo()
+
+    # Data setup.
+    client = Client(Number(1), String("dummy_name", max_len=20), date(2022, 2, 1), String("dummy_tel", max_len=20),
+                    String("dummy_descr", max_len=20), is_active=True)
+    client_repo.add(client)
+
+    activity = Activity(String("dummy_name", max_len=20), Currency(0.0), description=String("dummy_descr", max_len=20),
+                        charge_once=False)
+    activity_repo.add(activity)
+
+    subscription = Subscription(date(2022, 2, 2), client, activity)
+    client.add(subscription)
+    subscription_repo.add(subscription)
+
+    # Feature being tested.
+    system.charge(transaction_repo, subscription_repo, date(2022, 2, 2), client, activity, "dummy_method",
+                  responsible=String("dummy_resp", max_len=20))
+    assert len([t for t in transaction_repo.all()]) == 1 and client.up_to_date(date(2022, 2, 2), activity)
