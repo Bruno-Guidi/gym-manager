d65a3d42d8f47951135dde05020f7eb504a87d62
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-11 11:43:30 -0300

    Added new classmethod for Dialog, that creates a dialog that requires a responsible.

--- ui/widgets.py
+++ ui/widgets.py
@@ -3,15 +3,17 @@ from __future__ import annotations
 from datetime import date
 from typing import Type, Any, Callable
 
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QLineEdit, QWidget, QTextEdit, QHBoxLayout, QComboBox, QDialog, QVBoxLayout, QLabel,
     QPushButton, QDateEdit, QSpacerItem, QSizePolicy, QFrame)
 
+from gym_manager.core import constants
 from gym_manager.core.base import Validatable, ValidationError, String, Filter, ONE_MONTH_TD, DateGreater, DateLesser
 from gym_manager.core.persistence import FilterValuePair
-from ui.widget_config import fill_combobox, config_combobox, config_line, config_lbl, config_btn, config_layout, \
-    config_date_edit
+from ui.widget_config import (
+    fill_combobox, config_combobox, config_line, config_lbl, config_btn, config_layout,
+    config_date_edit)
 
 
 def valid_text_value(text: QTextEdit, max_len: int, optional: bool = False) -> tuple[bool, Any]:
@@ -230,6 +232,18 @@ class FilterHeader(QWidget):
 
 
 class Dialog(QDialog):
+    @classmethod
+    def confirm_with_resp(
+            cls, question: str, ok_btn_text: str | None = None, cancel_btn_text: str | None = None
+    ) -> tuple[bool, String]:
+        dialog = Dialog(title="Confirmar", text=question, show_cancel_btn=True, with_responsible=True)
+        if ok_btn_text is not None:
+            dialog.confirm_btn.setText(ok_btn_text)
+        if cancel_btn_text is not None:
+            dialog.cancel_btn.setText(cancel_btn_text)
+        dialog.exec_()
+        # noinspection PyTypeChecker
+        return dialog.confirmed, dialog.responsible_field.value()
 
     @classmethod
     def confirm(cls, question: str, ok_btn_text: str | None = None, cancel_btn_text: str | None = None) -> bool:
@@ -247,9 +261,9 @@ class Dialog(QDialog):
         dialog.exec_()
         return True
 
-    def __init__(self, title: str, text: str, show_cancel_btn: bool) -> None:
+    def __init__(self, title: str, text: str, show_cancel_btn: bool, with_responsible: bool = False) -> None:
         super().__init__()
-        self._setup_ui(title, text, show_cancel_btn)
+        self._setup_ui(title, text, show_cancel_btn, with_responsible)
         self.confirmed = False
         # noinspection PyUnresolvedReferences
         self.confirm_btn.clicked.connect(self.accept)
@@ -257,7 +271,7 @@ class Dialog(QDialog):
             # noinspection PyUnresolvedReferences
             self.cancel_btn.clicked.connect(self.reject)
 
-    def _setup_ui(self, title: str, text: str, show_cancel_btn: bool):
+    def _setup_ui(self, title: str, text: str, show_cancel_btn: bool, with_responsible: bool = False):
         self.setWindowTitle(title)
 
         self.layout = QVBoxLayout(self)
@@ -269,6 +283,20 @@ class Dialog(QDialog):
         self.text_lbl.setWordWrap(True)
         self.text_lbl.adjustSize()
         self.text_lbl.setMinimumSize(self.text_lbl.sizeHint())
+
+        # Responsible.
+        self.responsible_field: Field | None = None
+        if with_responsible:
+            self.responsible_layout = QHBoxLayout()
+            self.layout.addLayout(self.responsible_layout)
+
+            self.responsible_lbl = QLabel(self)
+            self.responsible_layout.addWidget(self.responsible_lbl)
+            config_lbl(self.responsible_lbl, "Responsable*")
+
+            self.responsible_field = Field(String, self, max_len=constants.CLIENT_NAME_CHARS)
+            self.responsible_layout.addWidget(self.responsible_field)
+            config_line(self.responsible_field, place_holder="Responsable", adjust_to_hint=False)
 
         # Vertical spacer.
         self.layout.addSpacerItem(QSpacerItem(20, 10, QSizePolicy.Minimum, QSizePolicy.MinimumExpanding))
@@ -292,6 +320,9 @@ class Dialog(QDialog):
         self.setMaximumSize(self.minimumWidth(), self.minimumHeight())
 
     def accept(self) -> None:
+        if self.responsible_field is not None and not self.responsible_field.valid_value():
+            return
+
         self.confirmed = True
         super().accept()
 
