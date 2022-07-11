ac00935f60bf58e12be1845a0f3a24dfc43ad385
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-11 14:59:04 -0300

    Added test case for BookingSystem.bookingAvailable(args) method.

--- test/test_booking.py
+++ test/test_booking.py
@@ -2,8 +2,8 @@ from datetime import date, time
 from typing import Generator
 
 from gym_manager.booking.core import (
-    Duration, BookingRepo, Booking, State, Court, FixedBooking, FixedBookingHandler)
-from gym_manager.core.base import Client
+    Duration, BookingRepo, Booking, State, Court, FixedBooking, FixedBookingHandler, BookingSystem, BOOKING_TO_HAPPEN)
+from gym_manager.core.base import Client, Activity, String, Currency
 from gym_manager.core.persistence import FilterValuePair
 
 
@@ -11,7 +11,7 @@ class MockBookingRepo(BookingRepo):
 
     def create(
             self, court: Court, client: Client, is_fixed: bool, state: State, when: date, start: time, end: time
-            ) -> Booking:
+    ) -> Booking:
         pass
 
     def update(self, booking: Booking, prev_state: State):
@@ -27,12 +27,22 @@ class MockBookingRepo(BookingRepo):
             court: str | None = None,
             filters: list[FilterValuePair] | None = None
     ) -> Generator[Booking, None, None]:
-        pass
+        # noinspection PyTypeChecker
+        to_yield = [
+            Booking("1", client=None, is_fixed=False, state=State(BOOKING_TO_HAPPEN), when=date(2022, 7, 11),
+                    start=time(8, 0), end=time(9, 0)),
+            Booking("1", client=None, is_fixed=False, state=State(BOOKING_TO_HAPPEN), when=date(2022, 7, 11),
+                    start=time(12, 0), end=time(13, 0)),
+            Booking("1", client=None, is_fixed=False, state=State(BOOKING_TO_HAPPEN), when=date(2022, 7, 11),
+                    start=time(16, 0), end=time(17, 0))
+        ]
+
+        for booking in to_yield:
+            yield booking
 
     def all_fixed(self) -> list[FixedBooking]:
         # noinspection PyTypeChecker
-        return [FixedBooking("1", client=None, start=time(9, 0), end=time(10, 0), day_of_week=0),
-                FixedBooking("1", client=None, start=time(10, 0), end=time(12, 0), day_of_week=0),
+        return [FixedBooking("1", client=None, start=time(10, 0), end=time(12, 0), day_of_week=0),
                 FixedBooking("1", client=None, start=time(13, 0), end=time(14, 0), day_of_week=0),
                 FixedBooking("1", client=None, start=time(15, 0), end=time(16, 0), day_of_week=0)]
 
@@ -61,9 +71,14 @@ def test_FixedBookingHandler_bookingAvailable():
     assert fixed_handler.booking_available(0, "2", time(8, 0), Duration(120, "2h"))
 
 
-def test_bookingAvailable_withNonFixed():
-    pass
+def test_BookingSystem_bookingAvailable():
+    dummy_activity = Activity(String("TestName", max_len=20), Currency(100), String("TestDescr", max_len=20))
+    booking_repo = MockBookingRepo()
+    # noinspection PyTypeChecker
+    booking_system = BookingSystem(dummy_activity, booking_repo, courts_names=("1", "2"), durations=(),
+                                   start=time(8, 0), end=time(18, 0), minute_step=60)
 
-
-def test_bookingAvailable_withFixedAndNonFixed():
-    pass
+    assert booking_system.booking_available(date(2022, 7, 11), "1", time(9, 0), Duration(60, "1h"))
+    assert booking_system.booking_available(date(2022, 7, 11), "1", time(17, 0), Duration(60, "1h"))
+    assert not booking_system.booking_available(date(2022, 7, 11), "1", time(9, 0), Duration(120, "2h"))
+    assert not booking_system.booking_available(date(2022, 7, 11), "1", time(15, 0), Duration(60, "1h"))
