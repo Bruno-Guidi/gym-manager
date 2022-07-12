3e7c0f194ed3c36fb558a2b13dacbb5373a5c413
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-12 16:26:14 -0300

    Tested fixed booking temporary cancellation.

--- test/test_booking.py
+++ test/test_booking.py
@@ -6,7 +6,7 @@ import pytest
 from gym_manager import peewee
 from gym_manager.booking.core import (
     Duration, BookingRepo, TempBooking, State, Court, FixedBooking, FixedBookingHandler, BookingSystem,
-    Booking, time_range, Block)
+    Booking, time_range, Block, Cancellation)
 from gym_manager.booking.peewee import SqliteBookingRepo, serialize_inactive_dates, deserialize_inactive_dates
 from gym_manager.core.base import Client, Activity, String, Currency, Transaction, Number, OperationalError
 from gym_manager.core.persistence import FilterValuePair
@@ -60,6 +60,9 @@ class MockBookingRepo(BookingRepo):
                 FixedBooking("1", client=None, start=time(13, 0), end=time(14, 0), day_of_week=0),
                 FixedBooking("1", client=None, start=time(15, 0), end=time(16, 0), day_of_week=0),
                 FixedBooking("2", client=None, start=time(15, 0), end=time(16, 0), day_of_week=1)]
+
+    def cancelled(self, page: int = 1, page_len: int = 10) -> Generator[Cancellation, None, None]:
+        pass
 
     def count(self, filters: list[FilterValuePair] | None = None) -> int:
         pass
@@ -308,3 +311,37 @@ def test_integration_registerCharge_fixedBooking():
     # The length of all temporal bookings is checked to ensure that the charge was registered.
     assert booking.transaction == transaction and len([b for b in booking_repo.all_temporal()]) == 1
     assert booking.transaction == [b for b in booking_repo.all_fixed()][0].transaction
+
+
+def test_integration_cancel_fixedBooking():
+    # Set up.
+    peewee.create_database(":memory:")
+
+    activity_repo = peewee.SqliteActivityRepo()
+    balance_repo = peewee.SqliteBalanceRepo()
+    transaction_repo = peewee.SqliteTransactionRepo()
+    client_repo = peewee.SqliteClientRepo(activity_repo, transaction_repo)
+    booking_repo = SqliteBookingRepo(tuple(), client_repo, transaction_repo)
+
+    dummy_client = Client(Number(1), String("TestCli", max_len=20), date(2022, 6, 6), date(2000, 1, 1),
+                          String("TestTel", max_len=20), String("TestDir", max_len=20))
+    client_repo.add(dummy_client)
+    dummy_activity = Activity(String("TestName", max_len=20), Currency(100), String("TestDescr", max_len=20))
+    # noinspection PyTypeChecker
+    booking_system = BookingSystem(dummy_activity, booking_repo, courts_names=("1", "2"), durations=(),
+                                   start=time(8, 0), end=time(18, 0), minute_step=60)
+
+    booking_date = date(2022, 7, 11)
+    booking = booking_system.book("1", dummy_client, True, booking_date, time(8, 0), Duration(60, "1h"))
+    transaction = transaction_repo.create("dummy_type", booking_date, dummy_activity.price, "dummy_method",
+                                          String("TestResp", max_len=20), "test_desc", dummy_client)
+
+    # Feature being test.
+    booking_system.cancel(booking, String("TestResp", max_len=20), booking_date, False)
+
+    all_fixed = [b for b in booking_repo.all_fixed()]
+    # noinspection PyUnresolvedReferences
+    assert (not booking.collides(time(8, 0), time(9, 0), booking_date)  # The time range is available to be booked.
+            and len(all_fixed) == 1  # The FixedBooking wasn't removed.
+            and (all_fixed[0].inactive_dates == [{"from": date(2022, 7, 11), "to": date(2022, 7, 18)}])
+            and len([c for c in booking_repo.cancelled()]) == 1)
