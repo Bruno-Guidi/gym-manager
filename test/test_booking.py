3d19f6399586b267c693b45d91fe0cc3e911855c
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-12 15:34:35 -0300

    Added more booking related test cases, and deleted old script with tests.

--- test/test_booking.py
+++ test/test_booking.py
@@ -1,12 +1,14 @@
 from datetime import date, time, datetime
 from typing import Generator
 
+import pytest
+
 from gym_manager import peewee
 from gym_manager.booking.core import (
     Duration, BookingRepo, TempBooking, State, Court, FixedBooking, FixedBookingHandler, BookingSystem,
-    Booking)
+    Booking, time_range, Block)
 from gym_manager.booking.peewee import SqliteBookingRepo
-from gym_manager.core.base import Client, Activity, String, Currency, Transaction, Number
+from gym_manager.core.base import Client, Activity, String, Currency, Transaction, Number, OperationalError
 from gym_manager.core.persistence import FilterValuePair
 
 
@@ -61,6 +63,81 @@ class MockBookingRepo(BookingRepo):
 
     def count(self, filters: list[FilterValuePair] | None = None) -> int:
         pass
+
+
+def test_timeRange():
+    assert [td for td in time_range(time(8, 0), time(8, 0), minute_step=30)] == [time(hour=8, minute=0)]
+
+    expected = [time(hour=8, minute=0), time(hour=8, minute=30)]
+    assert [td for td in time_range(time(8, 0), time(8, 30), minute_step=30)] == expected
+
+    expected = [time(hour=8, minute=0), time(hour=8, minute=30),
+                time(hour=9, minute=0), time(hour=9, minute=30)]
+    assert [td for td in time_range(time(8, 0), time(9, 30), minute_step=30)] == expected
+
+
+def test_TempBooking_collides():
+    # noinspection PyTypeChecker
+    b = TempBooking("dummy_court", client=None, start=time(8, 30), end=time(12, 0), when=None)
+
+    # start < b.start < end < b.end.
+    assert b.collides(start=time(8, 0), end=time(11, 0))
+    # start < b.start < end == b.end.
+    assert b.collides(start=time(8, 0), end=time(12, 0))
+    # start < b.start < b.end < end.
+    assert b.collides(start=time(8, 0), end=time(13, 0))
+    # start == b.start < end < b.end.
+    assert b.collides(start=time(8, 30), end=time(11, 0))
+    # start == b.start < end == b.end.
+    assert b.collides(start=time(8, 30), end=time(12, 0))
+    # start == b.start < b.end < end.
+    assert b.collides(start=time(9, 30), end=time(13, 0))
+    # start > b.start < end < b.end.
+    assert b.collides(start=time(9, 30), end=time(11, 0))
+    # start > b.start < end == b.end.
+    assert b.collides(start=time(9, 30), end=time(12, 0))
+    # start > b.start < b.end < end.
+    assert b.collides(start=time(9, 30), end=time(13, 0))
+    # start < end < b.start < b.end.
+    assert not b.collides(start=time(7, 0), end=time(8, 0))
+    # start < end <= b.start < b.end.
+    assert not b.collides(start=time(7, 0), end=time(8, 30))
+    # b.start < b.end < start < end.
+    assert not b.collides(start=time(13, 0), end=time(14, 0))
+    # b.start < b.end <= start < end.
+    assert not b.collides(start=time(12, 0), end=time(14, 0))
+
+
+def test_BookingSystem_blocks():
+    # noinspection PyTypeChecker
+    booking_system = BookingSystem(courts_names=("1", "2"), durations=(), start=time(8, 0), end=time(12, 0), minute_step=60,
+                                   activity=None, repo=MockBookingRepo())
+
+    # start is under the booking system start.
+    expected = [Block(0, time(8, 0), end=time(9, 0)), Block(1, time(9, 0), end=time(10, 0)),
+                Block(2, time(10, 0), end=time(11, 0)), Block(3, time(11, 0), end=time(12, 0))]
+    assert expected == [b for b in booking_system.blocks(start=time(7, 0))]
+
+    # start is over the booking system start.
+    expected = [Block(0, time(10, 0), end=time(11, 0)), Block(0, time(11, 0), end=time(12, 0))]
+    assert expected == [b for b in booking_system.blocks(start=time(9, 30))]
+
+    # start is over the booking system end
+    assert [] == [b for b in booking_system.blocks(start=time(12, 30))]
+
+
+def test_BookingSystem_blockRange():
+    # noinspection PyTypeChecker
+    booking_system = BookingSystem(courts_names=("1", "2"), durations=(), start=time(8, 0), end=time(12, 0), minute_step=60,
+                                   activity=None, repo=MockBookingRepo())
+
+    # start and end in range.
+    assert booking_system.block_range(time(8, 0), time(12, 0)) == (0, 4)
+    with pytest.raises(OperationalError):
+        # start out of range.
+        booking_system.block_range(time(7, 0), time(12, 0))
+        # end out of range.
+        booking_system.block_range(time(8, 0), time(12, 30))
 
 
 def test_FixedBooking_isActive():
