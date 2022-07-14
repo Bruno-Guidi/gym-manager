14a49e92e9b5e3fd0973a5c220ffa7a6bb3fbb9b
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-14 10:34:10 -0300

    Revert "Implemented dunder len method in String."
    This reverts commit 939e76e9110dd603eb2d748fac5a6300a6427ef9.

--- gym_manager/core/base.py
+++ gym_manager/core/base.py
@@ -144,9 +144,6 @@ class String(Validatable):
     def __hash__(self) -> int:
         return hash(self._value)
 
-    def __len__(self) -> int:
-        return len(self._value)
-
     def validate(self, value: str, **kwargs) -> str:
         """Validates the given *value*. If the validation succeeds, return the given *value*.
 
