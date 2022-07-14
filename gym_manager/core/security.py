db6cafd8bce11c09128175781ab68b4582f99999
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-14 11:32:41 -0300

    ResponsibleHandler is now an abstract class.
    This allows easy mocking when testing, and the possibility of swapping to another implementation if required. Old SecurityHandler is now SimpleSecurityHandler.

--- gym_manager/core/security.py
+++ gym_manager/core/security.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import abc
 import logging
 from typing import Callable, ClassVar
 
@@ -40,11 +41,9 @@ class log_responsible:
         """Init method.
 
         Args:
-            handler: handles security related things.
             action_tag: tag used to identify the function being performed.
             action_name: display name of the function being performed.
         """
-        self.handler = handler
         self.action_tag = action_tag
         self.action_name = action_name
 
@@ -59,30 +58,31 @@ class log_responsible:
         return wrapped
 
 
-class SecurityHandler:
-    _singleton: ClassVar[SecurityHandler] = None
+class SecurityHandler(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def current_responsible(self) -> String:
+        raise NotImplementedError
 
-    @classmethod
-    def get(cls, action_tags: set[str] | None = None, needs_responsible: set[str] | None = None) -> SecurityHandler:
-        """Returns a singleton instance of SecurityHandler. If it is None, then create one.
+    @current_responsible.setter
+    @abc.abstractmethod
+    def current_responsible(self, other: String):
+        raise NotImplementedError
 
-        Args:
-            action_tags: tags of existing actions in the system.
-            needs_responsible: tags of existing actions that need a responsible to be executed. It must be a subset of
-                *action_tags*.
-
-        Raises:
-            ValueError if *needs_responsible* is not a subset of *action_tags*, or if calling the method for the first
-                time without all arguments.
+    @abc.abstractmethod
+    def cant_perform_action(self, action_tag: str) -> bool:
+        """Returns True if the action with *action_tag* can't be performed.
         """
-        if SecurityHandler._singleton is None:
-            if action_tags is None or needs_responsible is None:
-                raise ValueError(
-                    "The first call of ResponsibleHandler.get(args) classmethod requires the arguments [action_tags="
-                    "{action_tags}] and [needs_responsible{needs_responsible}] to be not None.")
-            SecurityHandler._singleton = SecurityHandler(action_tags, needs_responsible)
-        return SecurityHandler._singleton
+        raise NotImplementedError
 
+    @abc.abstractmethod
+    def handle_action(self, action_level: str, action_name: str):
+        """Does whatever is needed after executing a given action.
+        """
+        raise NotImplementedError
+
+
+class SimpleSecurityHandler(SecurityHandler):
     def __init__(self, action_tags: set[str], needs_responsible: set[str]):
         """Init method.
 
@@ -98,11 +98,19 @@ class SecurityHandler:
             raise ValueError(f"Argument [needs_responsible={needs_responsible}] must be a subset of argument ["
                              f"action_tags={action_tags}].")
 
-        self.responsible = String("", optional=True, max_len=30)
+        self._responsible = String("", optional=True, max_len=30)
         self.action_tags = action_tags
         self._needs_responsible = needs_responsible
 
-    def invalid_state(self, action_tag: str) -> bool:
+    @property
+    def current_responsible(self) -> String:
+        return self._responsible
+
+    @current_responsible.setter
+    def current_responsible(self, other: String):
+        self._responsible = other
+
+    def cant_perform_action(self, action_tag: str) -> bool:
         """Returns True if *action_tag* needs a responsible to be executed and there is no responsible specified.
         """
         return action_tag in self._needs_responsible and len(self.responsible) == 0
