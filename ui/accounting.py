387da5b7f64be5122679f14381781497a9ffb014
BGuidi <brunoguidi.laboral@gmail.com>
2022-07-15 14:34:19 -0300

    Minor label text change.

--- ui/accounting.py
+++ ui/accounting.py
@@ -544,7 +544,7 @@ class ChargeUI(QDialog):
         # Responsible.
         self.responsible_lbl = QLabel(self)
         self.form_layout.addWidget(self.responsible_lbl, 3, 0)
-        config_lbl(self.responsible_lbl, "Responsable*")
+        config_lbl(self.responsible_lbl, "Responsable")
 
         self.responsible_field = Field(String, parent=self, optional=True, max_len=constants.CLIENT_NAME_CHARS)
         self.form_layout.addWidget(self.responsible_field, 3, 1)
