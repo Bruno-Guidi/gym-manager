7032e15bc4ea2d94143537a5f340e15623f78cd5
BGuidi <brunoguidi.laboral@gmail.com>
2022-08-02 11:10:14 -0300

    Implemented all(args) and update(args) methods of SqliteContactRepo.

--- gym_manager/contact/peewee.py
+++ gym_manager/contact/peewee.py
@@ -1,16 +1,16 @@
 from typing import Generator
 
-from peewee import Model, ForeignKeyField, CharField, IntegerField
+from peewee import Model, ForeignKeyField, CharField, IntegerField, JOIN
 
 from gym_manager.contact.core import ContactRepo, Contact
-from gym_manager.core.base import String, Client
-from gym_manager.core.persistence import LRUCache
+from gym_manager.core.base import String, Client, Number
+from gym_manager.core.persistence import LRUCache, ClientView
 from gym_manager.peewee import ClientTable, DATABASE_PROXY
 
 
 class ContactModel(Model):
     id = IntegerField(primary_key=True)
-    name = CharField(null=True)
+    c_name = CharField()
     tel1 = CharField()
     tel2 = CharField()
     direction = CharField()
@@ -35,18 +35,40 @@ class SqliteContactRepo(ContactRepo):
             client: Client | None = None
     ) -> Contact:
         client_id = None if client is None else client.id
-        record = ContactModel.create(name=name.as_primitive(), tel1=tel1.as_primitive(), tel2=tel2.as_primitive(),
+        record = ContactModel.create(c_name=name.as_primitive(), tel1=tel1.as_primitive(), tel2=tel2.as_primitive(),
                                      direction=direction.as_primitive(), description=description.as_primitive(),
                                      client_id=client_id)
         return Contact(record.id, name, tel1, tel2, direction, description, client)
 
     def update(self, contact: Contact):
-        pass
+        record = ContactModel.get_by_id(contact.id)
+        record.tel1 = contact.tel1
+        record.tel2 = contact.tel2
+        record.direction = contact.direction
+        record.description = contact.description
+        if contact.client is None:
+            record.c_name = contact.name
+        record.save()
 
     def remove(self, contact: Contact):
         pass
 
     def all(
-            self, page: int = 1, page_len: int | None = None, name: str | None = None
+            self, page: int = 1, page_len: int | None = None, name: String | None = None
     ) -> Generator[Contact, None, None]:
-        pass
+        query = ContactModel.select().join(ClientTable, JOIN.LEFT_OUTER)
+
+        if name is not None:
+            query = query.where((ContactModel.name == name.as_primitive())
+                                | (ClientTable.cli_name == name.as_primitive()))
+
+        if page_len is not None:
+            query = query.paginate(page, page_len)
+
+        for record in query:
+            client, client_record = None, record.client
+            if client_record is not None:
+                client = ClientView(client_record.id, client_record.cli_name, "SqliteContactRepo.all",
+                                    Number(client_record.dni if client_record.dni is not None else ""))
+            yield Contact(record.id, String(record.c_name), String(record.tel1), String(record.tel2),
+                          String(record.direction), String(record.description), client)
