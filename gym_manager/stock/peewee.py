bff6b3ec1cdb9b008d79a9a5b107d08d76809d69
BGuidi <brunoguidi.laboral@gmail.com>
2022-08-03 11:39:12 -0300

    Implemented SqliteItemRepo.create(args).

--- gym_manager/stock/peewee.py
+++ gym_manager/stock/peewee.py
@@ -1,10 +1,28 @@
+from peewee import Model, IntegerField, CharField
+
 from gym_manager.core.base import String, Number, Currency
+from gym_manager.peewee import DATABASE_PROXY
 from gym_manager.stock.core import ItemRepo, Item
 
 
+class ItemModel(Model):
+    code = IntegerField(primary_key=True)
+    item_name = CharField()
+    amount = IntegerField()
+    price = CharField()
+
+    class Meta:
+        database = DATABASE_PROXY
+
+
 class SqliteItemRepo(ItemRepo):
+    def __init__(self):
+        DATABASE_PROXY.create_tables([ItemModel])
+
     def create(self, name: String, amount: Number, price: Currency, is_fixed: bool = False) -> Item:
-        pass
+        record = ItemModel.create(item_name=name.as_primitive(), amount=amount.as_primitive(),
+                                  price=price.as_primitive())
+        return Item(record.code, name, amount, price)
 
     def remove(self, item: Item):
         pass
