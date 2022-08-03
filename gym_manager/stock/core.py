39adb4f603592a0a49989c5e3ecee602e25d795b
BGuidi <brunoguidi.laboral@gmail.com>
2022-08-03 10:49:40 -0300

    Implemented api to use to interact with items.

--- gym_manager/stock/core.py
+++ gym_manager/stock/core.py
@@ -1,6 +1,7 @@
 import abc
 
-from gym_manager.core.base import Currency, String, Number
+from gym_manager.core.api import CreateTransactionFn
+from gym_manager.core.base import Currency, String, Number, OperationalError
 
 
 class Item:
@@ -37,5 +38,44 @@ class ItemRepo(abc.ABC):
     def update(self, item: Item):
         raise NotImplementedError
 
+
+def create_item(item_repo: ItemRepo, name: String, amount: Number, price: Currency, is_fixed: bool = False) -> Item:
+    return item_repo.create(name, amount, price, is_fixed)
+
+
+def remove_item(item_repo: ItemRepo, item: Item):
+    item_repo.remove(item)
+
+
+def update_item(item_repo: ItemRepo, item: Item, name: String, price: Currency):
+    item.name = name
+    item.price = price
+    item_repo.update(item)
+
+
+def update_item_amount(
+        item_repo: ItemRepo, item: Item, amount: Number, update_cause: String) -> tuple[Item, Number, String]:
+    if amount > item.amount:
+        raise OperationalError(f"The [{item.code=}] does not have [{amount=}] units. It has [{item.amount=}]")
+
+    item.amount += amount
+    item_repo.update(item)
+
+    return item, amount, update_cause  # This is returned, so it can be logged by log_responsible decorator.
+
+
+def register_charge(
+        item_repo: ItemRepo, item: Item, amount: Number, create_transaction_fn: CreateTransactionFn
+) -> tuple[Item, Number]:
+    if amount > item.amount:
+        raise OperationalError(f"The [{item.code=}] does not have [{amount=}] units. It has [{item.amount=}]")
+
+    if not item.is_fixed:
+        item.amount -= amount
+
+    create_transaction_fn()
+    item_repo.update(item)
+
+    return item, amount  # This is returned, so it can be logged by log_responsible decorator.
 
 
