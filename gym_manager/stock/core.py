446655ea1de26faeac74dbfc2cffa9260c3f7470
BGuidi <brunoguidi.laboral@gmail.com>
2022-08-03 10:27:48 -0300

    Defined ItemRepo interface.

--- gym_manager/stock/core.py
+++ gym_manager/stock/core.py
@@ -1,3 +1,5 @@
+import abc
+
 from gym_manager.core.base import Currency, String
 
 
@@ -11,6 +13,20 @@ class Item:
 
     def total_price(self, units: int) -> Currency:
         return self.price.multiply_by_scalar(units)
+
+
+class ItemRepo(abc.ABC):
+    @abc.abstractmethod
+    def create(self, name: String, amount: int, price: Currency, is_fixed: bool = False) -> Item:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def remove(self, item: Item):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def update(self, item: Item):
+        raise NotImplementedError
 
 
 
