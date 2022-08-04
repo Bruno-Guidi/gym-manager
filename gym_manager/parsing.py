907a9a999b08ebb8f9dd1bbb578e74818be578cf
BGuidi <brunoguidi.laboral@gmail.com>
2022-08-04 12:16:56 -0300

    Now when parsing the backup, a new table is created that contains information about charges of the last 2 months.

--- gym_manager/parsing.py
+++ gym_manager/parsing.py
@@ -5,6 +5,7 @@ from sqlite3 import Connection
 
 from gym_manager.contact.core import ContactRepo
 from gym_manager.core.persistence import ActivityRepo, ClientRepo, SubscriptionRepo, TransactionRepo
+from gym_manager.old_app_info import OldChargesRepo
 
 
 def _create_temp_tables(db: Connection):
@@ -206,6 +207,21 @@ def _register_subscription_charging(
     subscription_repo.register_raw_charges(sub_charges)
 
 
+def _extract_charges(conn: Connection, since: date):
+    """Sums charges made after *since* for a given (month, year, activity, client). This is used to visualize if a
+    client owes money of a monthly subscription.
+    """
+    charges = (raw for raw in conn.execute(
+        "select c.nombre, a.descripcion, strftime('%m', p.fecha), strftime('%Y', p.fecha), sum(p.importe) "
+        "from pago p "
+        "inner join actividad a on p.id_actividad = a.id "
+        "inner join cliente c on p.id_cliente = c.id "
+        "where p.fecha >= (?) "
+        "group by strftime('%m', p.fecha), strftime('%Y', p.fecha), p.id_cliente, a.descripcion", (since,)))
+
+    OldChargesRepo.add_all(charges)
+
+
 def minus_n_months(date_: date, n: int) -> date:
     """Subtracts *n* months to *date_*.
     """
@@ -236,6 +252,10 @@ def parse(
     _insert_clients(conn, client_repo, contact_repo)
     _insert_subscriptions(conn, subscription_repo, since)
     _register_subscription_charging(conn, subscription_repo, transaction_repo, since)
+
+    # The following line will extract charges made in the current month and in the previous one.
+    OldChargesRepo.create_model()
+    _extract_charges(conn, since=minus_n_months(date.today(), n=1))
 
     conn.close()
 
