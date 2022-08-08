67576097e7109cdc2a13f0332048e0b15a175ea9
BGuidi <brunoguidi.laboral@gmail.com>
2022-08-08 12:25:54 -0300

    Replaced *client* fk with *client_name* column.

--- gym_manager/booking/peewee.py
+++ gym_manager/booking/peewee.py
@@ -31,7 +31,7 @@ def deserialize_inactive_dates(raw_inactive_dates: list[dict[str, str]]):
 class BookingTable(Model):
     when = DateTimeField()
     court = CharField()
-    client = ForeignKeyField(peewee.ClientTable, backref="bookings")
+    client_name = CharField()
     end = TimeField()
     is_fixed = BooleanField()
     transaction = ForeignKeyField(peewee.TransactionTable, backref="charged_booking", null=True)
@@ -45,7 +45,7 @@ class FixedBookingTable(Model):
     day_of_week = IntegerField()
     court = CharField()
     start = TimeField()
-    client = ForeignKeyField(peewee.ClientTable, backref="fixed_bookings")
+    client_name = CharField()
     end = TimeField()
     transaction = ForeignKeyField(peewee.TransactionTable, backref="charged_fixed_booking", null=True)
     first_when = DateField()
@@ -61,7 +61,7 @@ class CancelledLog(Model):
     id = IntegerField(primary_key=True)
     cancel_datetime = DateTimeField()
     responsible = CharField()
-    client = ForeignKeyField(peewee.ClientTable, backref="cancelled_bookings")
+    client_name = CharField()
     when = DateField(null=True)
     court = CharField()
     start = TimeField()
@@ -111,13 +111,13 @@ class SqliteBookingRepo(BookingRepo):
         if isinstance(booking, FixedBooking):
             booking: FixedBooking
             FixedBookingTable.create(day_of_week=booking.day_of_week, court=booking.court, start=booking.start,
-                                     client_id=booking.client.id, end=booking.end, first_when=booking.first_when,
+                                     client_name=booking.client_name, end=booking.end, first_when=booking.first_when,
                                      last_when=booking.when, inactive_dates=[])
             self.fixed_booking_cache[FixedBookingKey(booking.day_of_week, booking.court, booking.start)] = booking
         elif isinstance(booking, TempBooking):
             booking: TempBooking
             when = datetime.combine(booking.when, booking.start)
-            BookingTable.create(when=when, court=booking.court, client_id=booking.client.id, end=booking.end,
+            BookingTable.create(when=when, court=booking.court, client_name=booking.client_name, end=booking.end,
                                 is_fixed=False)
 
             self.temp_booking_cache[TempBookingKey(when, booking.court)] = booking
@@ -129,12 +129,12 @@ class SqliteBookingRepo(BookingRepo):
         if isinstance(booking, FixedBooking):
             # There is no problem in using replace method here, because no table has a fk that references this table.
             FixedBookingTable.replace(day_of_week=booking.day_of_week, court=booking.court, start=booking.start,
-                                      client_id=booking.client.id, end=booking.end,
+                                      client_name=booking.client_name, end=booking.end,
                                       transaction_id=booking.transaction.id, first_when=booking.first_when,
                                       last_when=booking.when, inactive_dates=booking.inactive_dates).execute()
             # Creates a TempBooking based on the FixedBooking, so the charging is registered.
-            booking = TempBooking(booking.court, booking.client, booking.start, booking.end, booking.when, transaction,
-                                  is_fixed=True)
+            booking = TempBooking(booking.court, booking.client_name, booking.start, booking.end, booking.when,
+                                  transaction, is_fixed=True)
         elif not isinstance(booking, TempBooking):
             raise PersistenceError(f"Argument 'booking' of [type={type(booking)}] cannot be persisted in "
                                    f"SqliteBookingRepo.")
@@ -142,7 +142,7 @@ class SqliteBookingRepo(BookingRepo):
         # There is no problem in using replace method here, because no table has a fk that references this table.
         # Replace method is used because there is an existing booking with the given pk, but with no transaction.
         BookingTable.replace(when=datetime.combine(booking.when, booking.start), court=booking.court,
-                             client_id=booking.client.id, end=booking.end, is_fixed=booking.is_fixed,
+                             client_name=booking.client_name, end=booking.end, is_fixed=booking.is_fixed,
                              transaction_id=transaction.id).execute()
 
     def cancel(self, booking: Booking, definitely_cancelled: bool = True):
@@ -154,8 +154,9 @@ class SqliteBookingRepo(BookingRepo):
             else:  # The FixedBooking is temporally cancelled.
                 transaction_id = None if booking.transaction is None else booking.transaction.id
                 FixedBookingTable.replace(day_of_week=booking.day_of_week, court=booking.court, start=booking.start,
-                                          client_id=booking.client.id, end=booking.end, transaction_id=transaction_id,
-                                          first_when=booking.first_when, last_when=booking.when,
+                                          client_name=booking.client_name, end=booking.end,
+                                          transaction_id=transaction_id, first_when=booking.first_when,
+                                          last_when=booking.when,
                                           inactive_dates=serialize_inactive_dates(booking.inactive_dates)).execute()
         elif isinstance(booking, TempBooking):  # A TempBooking is always definitely cancelled.
             when = datetime.combine(booking.when, booking.start)
@@ -167,10 +168,10 @@ class SqliteBookingRepo(BookingRepo):
             self, cancel_datetime: datetime, responsible: String, booking: Booking, definitely_cancelled: bool
     ):
         record = CancelledLog.create(cancel_datetime=cancel_datetime, responsible=responsible.as_primitive(),
-                                     client_id=booking.client.id, when=booking.when, court=booking.court,
+                                     client_name=booking.client_name, when=booking.when, court=booking.court,
                                      start=booking.start, end=booking.end, is_fixed=booking.is_fixed,
                                      definitely_cancelled=definitely_cancelled)
-        self.cancellation_cache[record.id] = Cancellation(record.id, cancel_datetime, responsible, booking.client,
+        self.cancellation_cache[record.id] = Cancellation(record.id, cancel_datetime, responsible, booking.client_name,
                                                           booking.when, booking.court, booking.start, booking.end,
                                                           booking.is_fixed, definitely_cancelled)
 
@@ -178,9 +179,6 @@ class SqliteBookingRepo(BookingRepo):
             self, when: date | None = None, court: str | None = None, filters: list[FilterValuePair] | None = None
     ) -> Generator[TempBooking, None, None]:
         bookings_q = BookingTable.select()
-
-        # The left outer join is required to obtain the client data needed in ClientView.
-        bookings_q = bookings_q.join(peewee.ClientTable, JOIN.LEFT_OUTER)
 
         if when is not None:
             year, month, day = when.year, when.month, when.day
@@ -197,25 +195,18 @@ class SqliteBookingRepo(BookingRepo):
         for record in prefetch(bookings_q, TransactionTable.select()):
             pk = TempBookingKey(record.court, record.when)
             if pk not in self.temp_booking_cache:
-                client_record, client = record.client, None
-                if client_record.id in self.client_view_cache:
-                    client = self.client_view_cache[client_record.id]
-                else:
-                    client = ClientView(client_record.id, String(client_record.cli_name),
-                                        created_by="SqliteBookingRepo.all_temporal",
-                                        dni=Number(client_record.dni if client_record.dni is not None else ""))
-
                 trans_record, transaction = record.transaction, None
                 if trans_record is not None:
                     transaction = self.transaction_repo.from_data(
                         trans_record.id, trans_record.type, trans_record.when, trans_record.amount, trans_record.method,
-                        trans_record.responsible, trans_record.description, client, trans_record.balance_id
+                        trans_record.responsible, trans_record.description, client=None,
+                        balance_date=trans_record.balance_id
                     )
 
                 when = record.when.date()
                 start = record.when.time()
-                self.temp_booking_cache[pk] = TempBooking(record.court, client, start, record.end, when, transaction,
-                                                          record.is_fixed)
+                self.temp_booking_cache[pk] = TempBooking(record.court, String(record.client_name), start, record.end,
+                                                          when, transaction, record.is_fixed)
                 logger.getChild(type(self).__name__).info(
                     f"Creating Booking [booking.when={when}, booking.court={record.court}, booking.start={start}] from "
                     f"queried data."
@@ -224,30 +215,19 @@ class SqliteBookingRepo(BookingRepo):
             yield self.temp_booking_cache[pk]
 
     def all_fixed(self) -> Generator[FixedBooking, None, None]:
-        # The left outer join is required to obtain the client data needed in ClientView.
-        bookings_q = FixedBookingTable.select().join(peewee.ClientTable, JOIN.LEFT_OUTER)
-
-        for record in prefetch(bookings_q, TransactionTable.select()):
+        for record in prefetch(FixedBookingTable.select(), TransactionTable.select()):
             pk = FixedBookingKey(record.day_of_week, record.court, record.start)
             if pk not in self.fixed_booking_cache:
-                client_record, client = record.client, None
-                if client_record.id in self.client_view_cache:
-                    client = self.client_view_cache[client_record.id]
-                else:
-                    client = ClientView(client_record.id, String(client_record.cli_name),
-                                        created_by="SqliteBookingRepo.all_fixed",
-                                        dni=Number(client_record.dni if client_record.dni is not None else ""))
-
                 transaction_record, transaction = record.transaction, None
                 if transaction_record is not None:
                     transaction = self.transaction_repo.from_data(
                         transaction_record.id, transaction_record.type, transaction_record.when,
                         transaction_record.amount, transaction_record.method, transaction_record.responsible,
-                        transaction_record.description, client, transaction_record.balance_id
+                        transaction_record.description, client=None, balance_date=transaction_record.balance_id
                     )
                 self.fixed_booking_cache[pk] = FixedBooking(
-                    record.court, client, record.start, record.end, record.day_of_week, record.first_when,
-                    record.last_when, deserialize_inactive_dates(record.inactive_dates), transaction
+                    record.court, String(record.client_name), record.start, record.end, record.day_of_week,
+                    record.first_when, record.last_when, deserialize_inactive_dates(record.inactive_dates), transaction
                 )
                 logger.getChild(type(self).__name__).info(
                     f"Creating Booking [booking.day_of_week={record.day_of_week},  booking.court={record.court}, "
@@ -260,8 +240,6 @@ class SqliteBookingRepo(BookingRepo):
             self, page: int = 1, page_len: int = 10, filters: list[FilterValuePair] | None = None
     ) -> Generator[Cancellation, None, None]:
         cancelled_q = CancelledLog.select()
-        # The left outer join is required because some client data is needed in the ClientView.
-        cancelled_q = cancelled_q.join(peewee.ClientTable, JOIN.LEFT_OUTER)
 
         if filters is not None:
             for filter_, value in filters:
@@ -269,16 +247,8 @@ class SqliteBookingRepo(BookingRepo):
 
         for record in cancelled_q.paginate(page, page_len).order_by(CancelledLog.cancel_datetime.desc()):
             if record.id not in self.cancellation_cache:
-                client_record, client = record.client, None
-                if client_record.id in self.client_view_cache:
-                    client = self.client_view_cache[client_record.id]
-                else:
-                    client = ClientView(client_record.id, String(client_record.cli_name),
-                                        created_by="SqliteBookingRepo.cancelled",
-                                        dni=Number(client_record.dni if client_record.dni is not None else ""))
-
                 self.cancellation_cache[record.id] = Cancellation(
-                    record.id, record.cancel_datetime, record.responsible, client,
+                    record.id, record.cancel_datetime, record.responsible, String(record.client_name),
                     record.when, record.court, record.start, record.end, record.is_fixed, record.definitely_cancelled
                 )
                 logger.getChild(type(self).__name__).info(
