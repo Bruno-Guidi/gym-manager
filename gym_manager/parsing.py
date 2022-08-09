37634c3a5dcea106c3fc5c1a41a0ebd0265b837b
BGuidi <brunoguidi.laboral@gmail.com>
2022-08-09 09:42:27 -0300

    Added todo

--- gym_manager/parsing.py
+++ gym_manager/parsing.py
@@ -207,7 +207,7 @@ def parse(
     # This balance is created so the transactions parsed are not included in the daily balance of the day when the
     # parsing is done.
     balance_repo.add(date.min, String("Admin"), {})
-    _register_subscription_charging(conn, subscription_repo, transaction_repo, since, date(2022, 7, 25))
+    _register_subscription_charging(conn, subscription_repo, transaction_repo, since, date(2022, 7, 25))  # ToDo change to date.today()
 
     conn.close()
 
